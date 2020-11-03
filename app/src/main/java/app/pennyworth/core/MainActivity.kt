0a406b3c9aa96bef457162cce0567845c85e4de0
Shalom Halbert <shalomhalbert@gmail.com>
2020-11-03 08:53:37 -0500

    Added todos

--- app/src/main/java/app/pennyworth/core/MainActivity.kt
+++ app/src/main/java/app/pennyworth/core/MainActivity.kt
@@ -6,24 +6,33 @@ import android.widget.Button
 import com.plaid.link.Plaid
 import com.plaid.link.configuration.LinkLogLevel
 import com.plaid.link.linkTokenConfiguration
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
     companion object {
+        //Todo (1/2): Replace link token
         const val PLAID_LINK_TOKEN = "link-sandbox-0e1b014c-b0e8-47e6-bece-e22e56f217c7"
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.openPlaidLinkButton).setOnClickListener {
-            val linkTokenConfiguration = linkTokenConfiguration {
-                token = PLAID_LINK_TOKEN
-                logLevel = LinkLogLevel.VERBOSE
-            }
+        findViewById<Button>(R.id.openPlaidLinkButton).setOnClickListener { onClickOpenPlaidLinkButton() }
+    }
 
-            val plaidHandler = Plaid.create(application, linkTokenConfiguration)
-
-            plaidHandler.open(this)
+    private fun onClickOpenPlaidLinkButton() = runBlocking {
+        val linkTokenConfiguration = linkTokenConfiguration {
+            token = PLAID_LINK_TOKEN
+            logLevel = LinkLogLevel.VERBOSE
         }
+
+        val plaidHandler = Plaid.create(application, linkTokenConfiguration)
+
+        //Todo (2/2): Adjust delay and find that there is no significant difference
+        delay(10000)
+
+        plaidHandler.open(this@MainActivity)
     }
 }
